731f92daf5fc50d938a381fcfd29aacd4d9ec0be
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2025-08-05 19:49:06 +0700

    data: move data in head to config

---
+++ src/data/konfigurasi.js
@@ -0,0 +1,6 @@
+const head = {
+	title: "Desa Wisata Taman Sendang Bandung",
+	description: "Temukan ketenangan dan keindahan alam asli di Desa Wisata Taman Sendang Bandung, Sleman, Yogyakarta."
+}
+
+export { head }
